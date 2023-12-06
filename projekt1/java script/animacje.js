326bab4ec97d8fd2727b6ffafd47cd0b50e749e6
Lenchestero <147376926+Lenchestero@users.noreply.github.com>
2023-12-06 17:41:52 +0100

    Update animacje.js

--- projekt1/java script/animacje.js
+++ projekt1/java script/animacje.js
@@ -1,23 +1,4 @@
-/*obserwuje jak skroluje się między dinozaurami i robi animacje*//*
-console.log("start");
-let observer=new IntersectionObserver((dino,observer)=>{
-    dino.filter(e=>e.isIntersecting).forEach(obiekt=>{
-        obiekt.target.classList.add("schowany");
-        observer.unobserve(obiekt.target);
-    });
-});
-console.log("main");
-
-const options={
-    threshold:0.8,
-};
-
-    document.querySelectorAll('dinozaury_specyfikacje').forEach(e=>{
-            observer.observe(e)
-        });
-        
-        console.log("end");*/
-
+/*obserwuje jak skroluje się między dinozaurami i robi animacje, dodając klase schowane*/
 const the_animation = document.querySelectorAll('.ruch')
 
 const observer = new IntersectionObserver((entries) => {
@@ -29,7 +10,7 @@ const observer = new IntersectionObserver((entries) => {
 },
     { threshold: 0.8
     });
-//
+
     for (let i = 0; i < the_animation.length; i++) {
     const elements = the_animation[i];
 
